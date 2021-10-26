2e591493ded70bc9d58d0641f58fb90abc954a85
Emir Moretti <emirmoretti@gmail.com>
2021-10-26 18:01:23 -0300

    initial commit

---
+++ src/main/java/grupo1/proyectoEggEdu/ProyectoEggEduApplication.java
@@ -0,0 +1,13 @@
+package grupo1.proyectoEggEdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEggEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEggEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/grupo1/proyectoEggEdu/ProyectoEggEduApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo1.proyectoEggEdu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEggEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
